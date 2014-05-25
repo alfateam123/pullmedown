3988d4efade5434e40156eedd7f7b0e685ce5a26
alfateam123 <alfateam123@gmail.com>
2014-05-25 22:41:44 +0200

    add an utility for page retrieving, just in case

--- utils.py
+++ utils.py
@@ -9,3 +9,13 @@ def store(url, pathname, overwrite=True):
 			for chunk in image_content.iter_content(1024*(10**3)):
 				fd.write(chunk)
 	print ('[utils.store]: url=', url)
+
+def get_page(url, is_json=False, method="get", parameters=None):
+	try:
+		r = {
+			"get" : requests.get,
+			"post": requests.post
+			}[method.lower()](url, data=parameters)
+	except KeyError:
+		raise ValueError("method passed is not GET or POST")
+	return r.json() if is_json else r.text
