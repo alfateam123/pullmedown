b8c03e780939d686ad0d9b3d55df03bcb1081ec9
alfateam123 <alfateam123@gmail.com>
2014-05-27 14:41:32 +0000

    port the MegaTokyo downloader uses the utility module. also, now dump/download command line arguments are available

--- src/mt_downloader.py
+++ src/mt_downloader.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-import requests, re, utils
+import re
+import utils
+import argparse
 
 """
 Downloader for MegaTokyo webcomic.
@@ -11,19 +13,19 @@ xrange = range if not ( 'xrange' in dir(__builtins__) ) else xrange
 strip_image = lambda text: re.findall('src\="strips\/(\d+\.\w+)"', text)
 
 def last_comic(download=True, return_number=False):
-    r = requests.get('http://megatokyo.com')
-    print (strip_image(r.text))
-    strip_number = int(strip_image(r.text)[0][:-4]) #removing .png || .gif
+    text = utils.get_page('http://megatokyo.com')
+    #print (strip_image(text))
+    strip_number = int(strip_image(text)[0][:-4]) #removing .png || .gif
     if download:
-        dump_single(strip_number, image_format=strip_image(r.text)[0][-4:])
+        dump_single(strip_number, image_format=strip_image(text)[0][-4:])
     if return_number:
         return strip_number
 
 def dump_single(number, image_format=None):
     if not image_format:
-        r = requests.get('http://megatokyo.com/strip/{0}'.format(number)) #retrieving the image format
-        print (strip_image(r.text))
-        strip_name = strip_image(r.text)[0]
+        text = utils.get_page('http://megatokyo.com/strip/{0}'.format(number)) #retrieving the image format
+        #print (strip_image(text))
+        strip_name = strip_image(text)[0]
     else:
         strip_name = str(number)+image_format
     utils.store('http://megatokyo.com/strips/{0}'.format(strip_name), strip_name, overwrite=False)
@@ -33,5 +35,16 @@ def dump_whole():
         dump_single(strip_num)
 
 if __name__=="__main__":
-    print ("last comic number is: ", last_comic(download=False, return_number=True))
-    dump_whole();
+    parser = argparse.ArgumentParser(description='MegaTokyo downloader')
+    parser.add_argument('action', metavar='action', nargs='?', help='Action (dump, download)')
+    parser.add_argument('number', metavar='number', nargs='?', help='Issue to be downloaded with the "download" action')
+    args = vars(parser.parse_args())
+
+    if args['action'] == 'dump':
+        dump_whole()
+    elif args['action'] == 'list':
+        print("{0} episodes of MegaTokyo have been released".format(last_comic(False, True)))
+    elif args['action'] == 'download' and args['number']:
+        dump_single(int(args['number']))
+    else:
+        print('Sounds like you did something wrong... try -h')
