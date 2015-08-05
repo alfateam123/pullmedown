23490179c4eb57a2f0ec975381af10c4b8f6c328
alfateam123 <alfateam123@gmail.com>
2015-08-05 22:53:54 +0200

    Wait, what?
    Starting to do some weird things with this repository.
    Things like, you know... refactoring :D

---
+++ setup.py
@@ -0,0 +1,33 @@
+#from distutils.core import setup
+from setuptools import setup
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='pullmedown',
+    version='0.0.1',
+    author='alfateam123',
+    author_email='alfateam123@gmail.com',
+    packages=['pullmedown'],
+    url='http://orion.wasp.ovh/winterthediplomat/pullmedown',
+    #license='LICENSE.txt',
+    description='downloadin\' all day long',
+    long_description=LONG_DESCRIPTION,
+    install_requires=[
+       "click"
+    ],
+    classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    entry_points={
+      'console_scripts': [
+        'pullmedown=pullmedown:main'
+      ]
+    },
+
+    test_suite="tests"
+)
