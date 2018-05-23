9868c56f36906eaef1ecc2c1a548206ab2b6d952
winter <alfateam123@gmail.com>
2018-05-23 23:42:26 +0200

    remove(hak5): remove support for hak5

--- pullmedown/__init__.py
+++ pullmedown/__init__.py
@@ -36,6 +36,5 @@ def main():
 
 from .musicforprogramming import musicforprogramming
 from .generate_cbz import generatecbz
-from .hak5down import hak5
 from .imgur_downloader import imgur
 from .goodbye_to_halos import goodbyetohalos
